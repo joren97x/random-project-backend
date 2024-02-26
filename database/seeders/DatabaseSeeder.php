f7749a8d1ff72954eda58172618b9a7d1fed0113
joren97x <sumagangjoren@gmail.com>
2024-02-26 12:35:15 +0000

    factory in dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Groupchat;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        Groupchat::factory(6)->create();
+        Product::factory(20)->create();
+        User::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
